76cd13c314358346264e047c13c3f477cfc359f3
Afif Nur Fauzi <alscheift@gmail.com>
2024-05-26 22:30:18 +0700

    comments the unused controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,32 +1,32 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\View\View;
+// use App\Models\User;
+// use Illuminate\Http\RedirectResponse;
+// use Illuminate\Http\Request;
+// use Illuminate\Validation\Rule;
+// use Illuminate\View\View;
 
-class RegisterController extends Controller
-{
-    public function create(): View
-    {
-        return view('register.create');
-    }
+// class RegisterController extends Controller
+// {
+//     public function create(): View
+//     {
+//         return view('register.create');
+//     }
 
-    public function store(): RedirectResponse
-    {
-        $attributes = request()->validate([
-            'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3|unique:users,username',
-            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'max:255', 'min:7']
-        ]);
+//     public function store(): RedirectResponse
+//     {
+//         $attributes = request()->validate([
+//             'name' => 'required|max:255',
+//             'username' => 'required|max:255|min:3|unique:users,username',
+//             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+//             'password' => ['required', 'max:255', 'min:7']
+//         ]);
 
-        $user = User::create($attributes);
+//         $user = User::create($attributes);
 
-        auth()->login($user);
-        return redirect('/')->with('success', 'Your account has been created.');
-    }
-}
+//         auth()->login($user);
+//         return redirect('/')->with('success', 'Your account has been created.');
+//     }
+// }
--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -1,51 +1,51 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace App\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
+// use Illuminate\Http\RedirectResponse;
+// use Illuminate\Validation\ValidationException;
+// use Illuminate\Http\Request;
+// use Illuminate\View\View;
 
-class SessionsController extends Controller
-{
-    //
-    public function create(): View
-    {
-        return view('sessions.create');
-    }
+// class SessionsController extends Controller
+// {
+//     //
+//     public function create(): View
+//     {
+//         return view('sessions.create');
+//     }
 
-    /**
-     * @throws ValidationException
-     */
-    public function store(): RedirectResponse
-    {
-        $attributes = request()->validate([
-            'email' => ['required'],
-            'password' => ['required']
-        ]);
+//     /**
+//      * @throws ValidationException
+//      */
+//     public function store(): RedirectResponse
+//     {
+//         $attributes = request()->validate([
+//             'email' => ['required'],
+//             'password' => ['required']
+//         ]);
 
-        if (
-            !auth()->attempt($attributes)
-            &&
-            !auth()->attempt([
-                'username' => $attributes['email'],
-                'password' => $attributes['password']
-            ])
-        ) {
-            throw ValidationException::withMessages([
-                'email' => 'Your provided credentials could not be verified. Please try again.'
-            ]);
-        }
+//         if (
+//             !auth()->attempt($attributes)
+//             &&
+//             !auth()->attempt([
+//                 'username' => $attributes['email'],
+//                 'password' => $attributes['password']
+//             ])
+//         ) {
+//             throw ValidationException::withMessages([
+//                 'email' => 'Your provided credentials could not be verified. Please try again.'
+//             ]);
+//         }
 
 
-        session()->regenerate();
-        return redirect('/')->with('success', 'Welcome Back!');
-    }
+//         session()->regenerate();
+//         return redirect('/')->with('success', 'Welcome Back!');
+//     }
 
-    public function destroy(): RedirectResponse
-    {
-        auth()->logout();
-        return redirect('/')->with('success', 'Goodbye!');
-    }
-}
+//     public function destroy(): RedirectResponse
+//     {
+//         auth()->logout();
+//         return redirect('/')->with('success', 'Goodbye!');
+//     }
+// }
