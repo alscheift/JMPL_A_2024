4692efe03a645267ca5a871f9724abac321a770e
Afif Nur Fauzi <alscheift@gmail.com>
2024-05-26 15:06:55 +0700

    unsafe auth: add created at and updated at to user creation

--- app/Http/Controllers/Auth/Breeze/RegisteredUserController.php
+++ app/Http/Controllers/Auth/Breeze/RegisteredUserController.php
@@ -39,6 +39,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         event(new Registered($user));
